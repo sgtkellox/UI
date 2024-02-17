0d1850708cf733b5868274311371ed85f8d9edc3
sgtkellox <felixlkeller@gmx.de>
2024-02-17 16:39:35 +0100

    added some classes

--- src/data/WSI.java
+++ src/data/WSI.java
@@ -1,5 +1,43 @@
 package data;
 
 public class WSI {
+	private int wsiWith;
+	private int wsiHeight;
+	private String path;
+	private int emptyBorderX;
+	private int emptyBorderY;
+	
+	
+	public int getWsiHeight() {
+		return wsiHeight;
+	}
+	public void setWsiHeight(int wsiHeight) {
+		this.wsiHeight = wsiHeight;
+	}
+	
+	public int getWsiWith() {
+		return wsiWith;
+	}
+	public void setWsiWith(int wsiWith) {
+		this.wsiWith = wsiWith;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public int getEmptyBorderX() {
+		return emptyBorderX;
+	}
+	public void setEmptyBorderX(int emptyBorderX) {
+		this.emptyBorderX = emptyBorderX;
+	}
+	public int getEmptyBorderY() {
+		return emptyBorderY;
+	}
+	public void setEmptyBorderY(int emptyBorderY) {
+		this.emptyBorderY = emptyBorderY;
+	}
 
 }
