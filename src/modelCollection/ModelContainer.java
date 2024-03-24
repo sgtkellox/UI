f82d23adbb8da35a9d64ab2e69d10a1ba304521b
sgtkellox <felixlkeller@gmx.de>
2024-03-25 00:05:56 +0100

    models

--- src/modelCollection/ModelContainer.java
+++ src/modelCollection/ModelContainer.java
@@ -1,5 +1,8 @@
 package modelCollection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -10,7 +13,7 @@ public class ModelContainer {
 	private static final ObservableList<ModelDefinition> models = FXCollections.observableArrayList();
 	
 	private ModelContainer() {
-		
+		addDefaultModels();
 	}
 	
 	public static ModelContainer instance() {
@@ -25,7 +28,9 @@ public class ModelContainer {
 	}
 	
 	private void addDefaultModels() {
-		models.add(new ModelDefinition("EfficientNet-V2", 10, ));
+		List<String> ngLabels = new ArrayList<>();
+		ngLabels.add(null)
+		models.add(new ModelDefinition("EfficientNet-V2", 10,384, "hallo",7,ngLabels ));
 	}
 
 }
